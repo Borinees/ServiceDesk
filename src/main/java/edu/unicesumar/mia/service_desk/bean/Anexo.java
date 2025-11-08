bf1a93f5bce030f3d524fe1276d97dafa6184791
Borinees <miarochaborin@gmail.com>
2025-11-08 11:11:48 -0300

    Create class Anexo
    Created the class Anexo for the info of any attachment

---
+++ src/main/java/edu/unicesumar/mia/service_desk/bean/Anexo.java
@@ -0,0 +1,47 @@
+package edu.unicesumar.mia.service_desk.bean;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "anexo", schema = "service_desk")
+@Getter
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Anexo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_anexo")
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_chamado", referencedColumnName = "id_chamado")
+    private Chamado chamado;
+
+    @Column(name = "nome_arquivo", length = 255)
+    @ToString.Include
+    private String nomeArquivo;
+
+    @Column(name = "caminho_arquivo", columnDefinition = "TEXT")
+    private String caminhoArquivo;
+
+    @Column(name = "data_envio", columnDefinition = "TIMESTAMP DEFAUL NOW()")
+    @ToString.Include
+    private LocalDateTime dataEnvio = LocalDateTime.now();
+
+    public Anexo() {}
+
+    public Anexo(Chamado chamado, String nomeArquivo, String caminhoArquivo) {
+        this.chamado = chamado;
+        this.nomeArquivo = nomeArquivo;
+        this.caminhoArquivo = caminhoArquivo;
+    }
+}
